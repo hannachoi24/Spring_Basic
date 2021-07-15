8722edf4a8a4abd623ff988f674b1132ffe74dc2
choihanna <hnc24@naver.com>
2021-07-15 23:39:23 +0900

    회원 및 주문과 할인 도메인개발과 테스트

---
+++ core/src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,30 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) { // psvm 입력하고 Enter침
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L; // 멤버를 먼저 저장해야되기 때문에 첫번 째로 작성해줌
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member); // memberService를 통해 메모리 객체에 넣어야함 그래야 주문해서 찾아 쓸 수 있기 때문
+
+        Order order = orderService.createOrder(memberId, "itemA",10000);
+
+        System.out.println("order = " + order);
+
+    }
+}
+
+/*
+위는 1차원적인 테스트 방법
+JUnit을 사용해 test 해보자 -> core/test/java/hello.core에 order 패키지 생성 order패키지에 OrderServiceTest클래스 파일 생성*/
