57cd79073a27cb3de1fa91b8c084e3126f729227
choihanna <hnc24@naver.com>
2021-07-31 01:00:00 +0900

    스프링 빈 조회 - 상속 관계

--- core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -41,7 +41,7 @@ public class ApplicationContextExtendsFindTest {
     } // 구체화에 의존하기 때문에 별로 좋지않은 코드
 
     @Test
-    @DisplayName("부모 타입으로 무도 조회하기")
+    @DisplayName("부모 타입으로 모두 조회하기")
     void findAllBeanByParentType() {
         Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
         assertThat(beansOfType.size()).isEqualTo(2);
@@ -51,7 +51,7 @@ public class ApplicationContextExtendsFindTest {
     }
 
         @Test
-        @DisplayName("부모 타입으로 무도 조회하기 - Object")
+        @DisplayName("부모 타입으로 모두 조회하기 - Object")
         void findAllBeanByObjectType() {
             Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
             for (String key : beansOfType.keySet()) {
